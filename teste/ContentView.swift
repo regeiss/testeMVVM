032f5a1d3e5132b4a4a099951d02730a68cba0eb
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2022-04-04 20:49:18 -0300

    Initial Commit

---
+++ teste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  teste
+//
+//  Created by Roberto Edgar Geiss on 04/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by Roberto Edgar Geiss on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
