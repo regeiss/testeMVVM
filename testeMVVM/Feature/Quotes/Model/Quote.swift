651079c48af20a0d972ac30ad640d04258787e95
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2022-04-12 16:39:26 -0300

    Update Quote.swift

--- testeMVVM/Feature/Quotes/Model/Quote.swift
+++ testeMVVM/Feature/Quotes/Model/Quote.swift
@@ -20,8 +20,5 @@ extension Quote
     static var dummyData: [Quote]
     {
         [
-            Quote(anime: "Anime 1", character: "Character 1", quote: "Random Quote 1")
-           
-        ]
-    }
+             Quote(userId: 1, id: 1,title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")]
 }
